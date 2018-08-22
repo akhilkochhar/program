4abd221038301872b795eceef441bdf54c425351
akhilkochhar <42261211+akhilkochhar@users.noreply.github.com>
2018-08-22 09:31:38 +0530

    Create second.java
    second prog

---
+++ second.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Jka
+{
+public static void main(String[] x)
+
+{
+
+Scanner i=new Scanner(System.in);
+
+  
+String s;
+  
+s=i.nextLine();
+
+  System.out.println(s);
+
+
+}
+
+}
